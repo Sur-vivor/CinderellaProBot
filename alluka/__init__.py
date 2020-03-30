e7bbad5ffe2221e2f3521fe05912f24c9b44a6c8
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-03-30 10:27:36 +0000

    Update __init__.py

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -129,7 +129,7 @@ else:
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
 
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    DB_URI = Config.DB_URI
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
