0d21567196209ce109a02868f7eda1140d66bc58
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-04-03 02:59:54 +0000

    Create feedback.py

---
+++ alluka/modules/feedback.py
@@ -0,0 +1,44 @@
+import html
+from telegram import Update, Bot, ParseMode
+from telegram.ext import run_async
+from alluka.modules.disable import DisableAbleCommandHandler
+from alluka import dispatcher
+from requests import get
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@run_async
+def feedback(bot: Bot, update: Update):
+  name = update.effective_message.from_user.first_name
+  message = update.effective_message
+  userid=message.from_user.id
+  text = message.text[len('/feedback '):]
+   
+
+  feed_text = f"αℓℓυкα's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
+  
+
+  bot.send_message(-1001480701200, feed_text, parse_mode=ParseMode.MARKDOWN)
+ 
+  text = html.escape(text)
+  reply_text=f"Thankyou for giving us your feedback."
+  message.reply_text(reply_text, reply_markup=InlineKeyboardMarkup(
+                                                [[InlineKeyboardButton(text="You can see your feedback here",url="https://telegram.dog/allukabotfeeds")]]))
+                                               
+  
+
+  
+
+
+
+__help__ = """
+ - /feedback : You can give us your feedbacks 
+               can can see your feeds @allukabotfeeds here.
+"""
+
+__mod_name__ = "feedback"
+
+feed_handle = DisableAbleCommandHandler("feedback", feedback)
+
+dispatcher.add_handler(feed_handle)
