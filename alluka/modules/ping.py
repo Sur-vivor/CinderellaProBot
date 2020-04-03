606ff4b5efc805f9f5567b1ac2ddb89659b00b05
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-04-03 05:15:12 +0000

    Update ping.py

--- alluka/modules/ping.py
+++ alluka/modules/ping.py
@@ -76,11 +76,16 @@ def ping(bot: Bot, update: Update):
 
     telegram_ping = ping_func(["Telegram"])[0].split(": ", 1)[1]
     uptime = get_readable_time((time.time() - StartTime))
+    
+    allukaping = send_message(update.effective_message, "Pong!")
+
 
     reply_msg = "PONG!!\n<b>Time Taken:</b> <code>{}</code>" \
                 "\n<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime)
 
-    update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
+    context.bot.editMessageText(chat_id=update.effective_chat.id, message_id=allukaping.message_id,
+                        text=(update.effective_message,reply_msg),parse_mode=ParseMode.HTML)
+   # update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
 
 
 __help__ = """
