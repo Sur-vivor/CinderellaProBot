3cac6f55bc0fab8189b0c706712dca406e43cd43
Anil chauhan <62683938+anilchauhanxda@users.noreply.github.com>
2020-04-18 10:14:42 +0000

    Update misc.py

--- alluka/modules/helper_funcs/misc.py
+++ alluka/modules/helper_funcs/misc.py
@@ -51,17 +51,17 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                                     callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
              in module_dict.values()])
 
-    pairs = list(zip(modules[::3], modules[1::3],modules[3::]))
+    pairs = list(zip(modules[::2], modules[1::2]))
 
     if len(modules) % 2 == 1:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 5)
+    max_num_pages = ceil(len(pairs) / 7)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 5:
-        pairs = pairs[modulo_page * 5:5 * (modulo_page + 1)] + [
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
              EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
