8b1ee5bf6dae07bb2175ab039551a534bbe5bbe7
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-19 23:10:18 +0530

    Update music.py

--- alluka/modules/music.py
+++ alluka/modules/music.py
@@ -43,7 +43,7 @@ def music(bot: Bot, update: Update, args):
 
 
 
-	r = requests.get(f'https://www.pointmp3.com/api/framed/video_id/mp3/AIzaSyDxmk_iusdpHuj5VfFnqyvweW1Lep0j2oc')
+	r = requests.get(f'https://api.pointmp3.com/dl/{video_id}?format=mp4')
 	
 
 	json1_response = r.json()
