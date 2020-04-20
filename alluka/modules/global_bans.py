d7291c7af3204a0dcc067287ef58d328d39a6605
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-20 15:54:21 +0530

    Update global_bans.py

--- alluka/modules/global_bans.py
+++ alluka/modules/global_bans.py
@@ -132,7 +132,8 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS, 
                   "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                 html=True)
-    message.reply_text("Person has been gbanned.")
+    message.reply_text("{},{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
+                html=True)
 
 
 @run_async
