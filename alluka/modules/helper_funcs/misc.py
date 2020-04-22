e0d7a16fae3e63c5c60ff55462af50dd266eaa84
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-22 17:25:05 +0530

    Add files via upload

--- alluka/modules/helper_funcs/misc.py
+++ alluka/modules/helper_funcs/misc.py
@@ -63,7 +63,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
+                EqInlineKeyboardButton("🏡 Home", callback_data="bot_start"),
              EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
