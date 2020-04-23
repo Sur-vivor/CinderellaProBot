31229b60f2a71f16e6ba4819e2d4bdee60b37bd9
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-23 12:54:40 +0530

    Update misc.py

--- alluka/modules/helper_funcs/misc.py
+++ alluka/modules/helper_funcs/misc.py
@@ -67,7 +67,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("🏡 Home", callback_data="bot_start")]]
 
 
     return pairs
