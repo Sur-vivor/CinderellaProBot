d960aecc409c90f3e6375b74a70cd71b2a42ccc2
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-25 23:56:24 +0530

    Add files via upload

--- alluka/modules/covid_tracker.py
+++ alluka/modules/covid_tracker.py
@@ -3,92 +3,119 @@ from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
 from alluka import dispatcher
 
-import requests
-from parsel import Selector
-
+import os
 import json
+import requests
+from tabulate import tabulate
 from urllib.request import urlopen
 
+def sign_delta(delta_var):
+    if delta_var < 0:
+        delta_var = str(format(delta_var, ',d'))
+    else:
+        delta_var = '+' + str(format(delta_var, ',d'))
+    return delta_var
+
+@run_async
 def cov(bot: Bot, update: Update):
-    country = ''
-    confirmed = 0
-    deceased = 0
-    recovered = 0
     message = update.effective_message
-    selected = (''.join([message.text.split(' ')[i] + ' ' for i in range(1, len(message.text.split(' ')))])).strip()
-    url_global = "https://ncov2019.live"
-    text_global = requests.get(url_global).text
-    selector_global = Selector(text = text_global)
-    table = selector_global.css('#sortable_table_Global')
-    rows = table.css('tr')
-    if not selected:
-        country = country = rows[1].css('.text--gray::text').getall()[0].strip()
-        confirmed = rows[1].css('.text--green::text').getall()[0].strip()
-        deceased = rows[1].css('.text--red::text').getall()[0].strip()
-        recovered = rows[1].css('.text--blue::text').getall()[0].strip()
-    else:
-        for row in rows[2:]:
-            country = row.css('.text--gray::text').getall()[1].strip()
-            if country.lower() == selected.lower():
-                confirmed = row.css('.text--green::text').getall()[0].strip()
-                deceased = row.css('.text--red::text').getall()[0].strip()
-                recovered = row.css('.text--blue::text').getall()[0].strip()
-                break
-            country = ''
-
-    if not country:
-        country = selected
-
-    bot.send_message(
-        message.chat.id,
-        '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ ncov2019.live' % (country, confirmed, deceased, recovered),
-        parse_mode = ParseMode.MARKDOWN,
-        disable_web_page_preview = True
-    )
-
-def covindia(bot: Bot, update: Update):
-    message = update.effective_message
-    state = ''
     confirmed = 0
+    confirmed_delta = 0
     deceased = 0
+    deceased_delta = 0
+    active = 0
+    active_delta = 0
     recovered = 0
-    state_input = ''.join([message.text.split(' ')[i] + ' ' for i in range(1, len(message.text.split(' ')))]).strip()
-    if state_input:
-        url_india = 'https://api.covid19india.org/data.json'
-        json_url = urlopen(url_india)
-        state_dict = json.loads(json_url.read())
-        for sdict in state_dict['statewise']:
-            if sdict['state'].lower() == state_input.lower():
-                confirmed = sdict['confirmed']
-                deceased = sdict['deaths']
-                recovered = sdict['recovered']
-                state = sdict['state']
-                break
-    
-    if state:
+    recovered_delta = 0
+    mortality_rate = 0
+    recovery_rate = 0
+    country_input = ''
+    state_input = ''
+    district_input = ''
+
+    loc_input = message.text.split(',')
+    if len(loc_input) > 2:
+        district_input = loc_input[2].strip()
+    if len(loc_input) > 1:
+        state_input = loc_input[1].strip()
+    if len(loc_input) > 0:
+        country_input = loc_input[0][4:].strip()
+
+    try:
+        url_global = "https://bing.com/covid/place"
+        json_response = requests.get(url_global)
+        global_dict = json.loads(json_response.text)
+        
+        target = {}
+
+        if country_input:
+            for country in global_dict['areas']:
+                if country['displayName'].lower() == country_input.lower():
+                    if state_input:
+                        for state in country['areas']:
+                            if state['displayName'].lower() == state_input.lower():
+                                if district_input:
+                                    for district in state['areas']:
+                                        if district['displayName'].lower() == district_input.lower():
+                                            target = district
+                                else:
+                                    target = state
+                    else:
+                        target = country
+        else:
+            target = global_dict
+
+        if not target:
+            bot.send_message(
+                message.chat.id,
+                'Data unavailable for %s!' % (message.text[4:].strip())
+            )
+            return
+
+        confirmed = int(target['totalConfirmed'] or 0)
+        confirmed_delta = int(target['totalConfirmedDelta'] or 0)
+        deceased = int(target['totalDeaths'] or 0)
+        deceased_delta = int(target['totalDeathsDelta'] or 0)
+        recovered = int(target['totalRecovered'] or 0)
+        recovered_delta = int(target['totalRecoveredDelta'] or 0)
+        active = confirmed - deceased - recovered
+        active_delta = confirmed_delta - deceased_delta - recovered_delta
+
+        mortality_rate = (deceased / confirmed) * 100
+        recovery_rate = (recovered / confirmed) * 100
+
+        location = target['displayName'].upper()
+
         bot.send_message(
             message.chat.id,
-            '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
+            '`COVID-19 Tracker:` *%s*\n\n' % location.upper() +
+            '*Confirmed:* %s _(%s)_\n' % (format(confirmed, ',d'), sign_delta(confirmed_delta)) +
+            '*Active:* %s _(%s)_\n' % (format(active, ',d'), sign_delta(active_delta)) +
+            '*Deceased:* %s _(%s)_\n' % (format(deceased, ',d'), sign_delta(deceased_delta)) +
+            '*Recovered:* %s _(%s)_\n\n' % (format(recovered, ',d'), sign_delta(recovered_delta)) +
+            '*Mortality rate:* %s%%\n' % round(mortality_rate, 2) +
+            '*Recovery rate:* %s%%\n\n' % round(recovery_rate, 2) +
+            '[Powered by Bing.](https://bing.com/covid)',
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
-    else:
+        return
+
+    except:
         bot.send_message(
             message.chat.id,
-            'You need to specify a valid Indian state!',
-            parse_mode = ParseMode.MARKDOWN,
-            disable_web_page_preview = True
+            'Unable to contact the Bing COVID-19 Data API. Try again in a while.'
         )
+        return
+
 
 __help__ = """
- - /cov <country>: Get real time COVID-19 stats for the input country
- - /covindia <state>: Get real time COVID-19 stats for the input Indian state
+ - /cov <country> <state> <locality>: Get real time COVID-19 stats for the input location.
+ - /cov top <n(integer)>: Get the top n countries with the highest confirmed cases.
 """
 
 __mod_name__ = 'COVID-19 Tracker'
 
 COV_HANDLER = CommandHandler('cov', cov)
-COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
 
 dispatcher.add_handler(COV_HANDLER)
-dispatcher.add_handler(COV_INDIA_HANDLER)
