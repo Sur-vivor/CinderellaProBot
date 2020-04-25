b2892233396c514b146c41a00d9cda532b544c7e
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-26 01:17:31 +0530

    Update feds.py

--- alluka/modules/feds.py
+++ alluka/modules/feds.py
@@ -451,7 +451,7 @@ def fed_ban(bot: Bot, update: Update, args: List[str]):
             return
         x = sql.fban_user(fed_id, user_id, user_chat.first_name, user_chat.last_name, user_chat.username, reason)
         if not x:
-            message.reply_text("Failed to ban from the federation! If this problem persists, reach out to us @allukatm.")
+            message.reply_text("Failed to ban from the federation! If this problem persists, reach out to us @CinderellaHelp.")
             return
 
         fed_chats = sql.all_fed_chats(fed_id)
@@ -490,7 +490,7 @@ def fed_ban(bot: Bot, update: Update, args: List[str]):
 
     x = sql.fban_user(fed_id, user_id, user_chat.first_name, user_chat.last_name, user_chat.username, reason)
     if not x:
-        message.reply_text("Failed to ban from the federation! If this problem persists, reach out to us @allukatm.")
+        message.reply_text("Failed to ban from the federation! If this problem persists, reach out to us @CinderellaHelp.")
         return
 
     fed_chats = sql.all_fed_chats(fed_id)
