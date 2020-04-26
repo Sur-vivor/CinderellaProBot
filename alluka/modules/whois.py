a2683ca5a92a78afd4934b5d9d3887f1fccc05fa
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-26 23:17:38 +0530

    Update whois.py

--- alluka/modules/whois.py
+++ alluka/modules/whois.py
@@ -40,7 +40,9 @@ def info(bot: Bot, update: Update, args: List[str]):
 
     else:
         return
-
+    
+    del_msg = msg.reply_text("Hold tight while I steal some data from <b>FBI Database</b>...", parse_mode=ParseMode.HTML)
+    
     text = (f"<b>User Information:</b>\n"
             f"🆔: <code>{user.id}</code>\n"
             f"👤Name: {html.escape(user.first_name)}")
