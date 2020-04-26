64f308a5a9f86d72c498e4de1237d6a5606ab27f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-26 19:26:13 +0530

    Update ud.py

--- alluka/modules/ud.py
+++ alluka/modules/ud.py
@@ -18,7 +18,7 @@ __help__ = """
  - /ud:{word} Type the word or expression you want to search use. like /ud telegram Word: Telegram Definition: A once-popular system of telecommunications, in which the sender would contact the telegram service and speak their [message] over the [phone]. The person taking the message would then send it, via a teletype machine, to a telegram office near the receiver's [address]. The message would then be hand-delivered to the addressee. From 1851 until it discontinued the service in 2006, Western Union was the best-known telegram service in the world.
 """
 
-__mod_name__ = "Urban dictionary"
+__mod_name__ = "Urban Dictionary"
   
 ud_handle = DisableAbleCommandHandler("ud", ud)
 
