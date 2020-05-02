8871a0286fe1d379bfe53f57cc7446b191e38bfc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-02 16:04:37 +0530

    Update covid_tracker.py

--- alluka/modules/covid_tracker.py
+++ alluka/modules/covid_tracker.py
@@ -42,14 +42,41 @@ def covindia(bot: Bot, update: Update):
             parse_mode = ParseMode.MARKDOWN,
             disable_web_page_preview = True
         )
+@run_async
+def corona(bot: Bot, update: Update):
+    message = update.effective_message
+    device = message.text[len('/corona '):]
+    fetch = get(f'https://coronavirus-tracker-api.herokuapp.com/all')
+
+    if fetch.status_code == 200:
+        usr = fetch.json()
+        data = fetch.text
+        parsed = json.loads(data)
+        total_confirmed_global = parsed["latest"]["confirmed"]
+        total_deaths_global = parsed["latest"]["deaths"]
+        total_recovered_global = parsed["latest"]["recovered"]
+        active_cases_covid19 = total_confirmed_global - total_deaths_global - total_recovered_global
+        reply_text = ("*Corona Stats🦠:*\n"
+        "Total Confirmed: `" + str(total_confirmed_global) + "`\n"
+        "Total Deaths: `" + str(total_deaths_global) + "`\n"
+        "Total Recovered: `" + str(total_recovered_global) +"`\n"
+        "Active Cases: `"+ str(active_cases_covid19) + "`")
+        message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+        return
+
+    elif fetch.status_code == 404:
+        reply_text = "The API is currently down."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 __help__ = """
- 
+ - /covid: get worldwide corona status
  - /covindia <state>: Get real time COVID-19 stats for the input Indian state
 """
 
 __mod_name__ = 'COVID-19 Tracker'
 
 COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
-
+CORONA_HANDLER = DisableAbleCommandHandler("covid", corona, admin_ok=True)
+dispatcher.add_handler(CORONA_HANDLER)
 dispatcher.add_handler(COV_INDIA_HANDLER)
