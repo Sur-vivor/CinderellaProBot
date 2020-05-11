dac99f50a6e0456ab86b2bc98b623878a4b68099
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-11 18:28:44 +0530

    Update feds.py

--- alluka/modules/feds.py
+++ alluka/modules/feds.py
@@ -1598,16 +1598,18 @@ def is_user_fed_owner(fed_id, user_id):
 @run_async
 def welcome_fed(bot, update):
     chat = update.effective_chat
-    user = update.effective_user 
+    user = update.effective_user
 
     fed_id = sql.get_fed_id(chat.id)
-    fban, fbanreason, fbantime = sql.get_fban_user(fed_id, user.id)
+    fban, fbanreason = sql.get_fban_user(fed_id, user.id)
     if fban:
-        update.effective_message.reply_text("This user is banned in current federation! I will remove him.")
+        update.effective_message.reply_text(
+            "This user is banned in current federation! I will remove him.")
         bot.kick_chat_member(chat.id, user.id)
         return True
+    else:
+        return False
 
-    return False
 	
 def __stats__():
     all_fbanned = sql.get_all_fban_users_global()
