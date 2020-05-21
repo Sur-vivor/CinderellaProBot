7a9bfa2326e46cb628722afb1c1a98123a276123
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-05-21 20:58:40 +0530

    Update cas_api.py

--- alluka/modules/helper_funcs/cas_api.py
+++ alluka/modules/helper_funcs/cas_api.py
@@ -1,28 +1,31 @@
 import urllib.request as url
 import json
 import datetime
-import requests
 
-
-VERSION = "1.3.3"
+DEVUSER = "" #user ID that is banned for testing
+VERSION = "1.3.2 - DEV"
 CAS_QUERY_URL = "https://api.cas.chat/check?user_id="
 DL_DIR = "./csvExports"
 
 def get_user_data(user_id):
-    with requests.request('GET', CAS_QUERY_URL + str(user_id)) as userdata_raw:
-        userdata = json.loads(userdata_raw.text)
+    with url.urlopen(CAS_QUERY_URL + str(user_id)) as userdata_raw:
+        userdata = json.loads(userdata_raw.read().decode())
         return userdata
 
 def isbanned(userdata):
     return userdata['ok']
 
 def banchecker(user_id):
+    if(str(user_id) == DEVUSER):
+        return True
     return isbanned(get_user_data(user_id))
 
 def vercheck() -> str:
     return str(VERSION)
 
 def offenses(user_id):
+    if str(user_id) == DEVUSER:
+        return "TEST USER"
     userdata = get_user_data(user_id)
     try:
         offenses = userdata['result']['offenses']
@@ -31,6 +34,8 @@ def offenses(user_id):
         return None
     
 def timeadded(user_id):
+    if str(user_id) == DEVUSER:
+        return "TEST USER - NEVER"
     userdata = get_user_data(user_id)
     try:
         timeEp = userdata['result']['time_added']
