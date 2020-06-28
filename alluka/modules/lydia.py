daaef6dee5fdbab0a8d735284a7c252b3d7d7cb1
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 22:20:02 +0530

    Update lydia.py

--- alluka/modules/lydia.py
+++ alluka/modules/lydia.py
@@ -87,7 +87,7 @@ def lydia(bot: Bot, update: Update):
             bot.send_message(OWNER_ID, f"lydia error: {e} occurred in {chat_id}!")
                     
 
-__mod_name__ = "Lydia"
+__mod_name__ = "LYDIA/CHATBOT"
 
 __help__ = """
 Commands
