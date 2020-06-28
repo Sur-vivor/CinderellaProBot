5f546b2760e4d6b67fc72db47f2a06d6e68f926b
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 21:56:20 +0530

    Update global_bans.py

--- alluka/modules/global_bans.py
+++ alluka/modules/global_bans.py
@@ -411,7 +411,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 - /cleangb : To Remove all Deleted Accounts from gban list
 """
 
-__mod_name__ = "Global Bans"
+__mod_name__ = "GLOBAL BANS"
 
 GBAN_HANDLER = CommandHandler("gban", gban, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
