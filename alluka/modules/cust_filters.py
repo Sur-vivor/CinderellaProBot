786669741bfc7db57dc62f4f11395b5e57954b2b
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 21:21:02 +0530

    Update cust_filters.py

--- alluka/modules/cust_filters.py
+++ alluka/modules/cust_filters.py
@@ -275,7 +275,7 @@ doin?
  - /stop <filter keyword>: stop that filter.
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "FILTERS"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
