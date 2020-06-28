19fa844e2833c1833a561d9e067aa58c83cd9c76
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 22:37:15 +0530

    Update reverseimage.py

--- alluka/modules/reverseimage.py
+++ alluka/modules/reverseimage.py
@@ -184,7 +184,7 @@ __help__ = """
 - /reverse: Does a reverse image search of the media which it was replied to.
 """
 
-__mod_name__ = "Reverse Image search"
+__mod_name__ = "REVERSE IMAGE SEARCH"
 
 REVERSE_HANDLER = DisableAbleCommandHandler("reverse", reverse, pass_args=True, admin_ok=True)
 
