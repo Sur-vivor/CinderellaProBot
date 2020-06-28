6d3107c5335f5e2549efd37ebd9b73ee2efc8101
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 22:27:06 +0530

    Update notes.py

--- alluka/modules/notes.py
+++ alluka/modules/notes.py
@@ -248,7 +248,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
  - /clear <notename>: clear note with this name
 """
 
-__mod_name__ = "Notes"
+__mod_name__ = "NOTES"
 
 GET_HANDLER = CommandHandler("get", cmd_get, pass_args=True)
 HASH_GET_HANDLER = RegexHandler(r"^#[^\s]+", hash_get)
