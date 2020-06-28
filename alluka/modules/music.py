d4eec7e59ac773b2729a7941ed5c479fdbcb4248
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 22:25:02 +0530

    Update music.py

--- alluka/modules/music.py
+++ alluka/modules/music.py
@@ -80,7 +80,7 @@ __help__ = """ Youtube audio Downloader
 It will be up soon.
 
 """
-__mod_name__ = "Music" 
+__mod_name__ = "MP3 DOWNLOADER" 
 
 music_handler = CommandHandler('music', music, pass_args=True)
 dispatcher.add_handler(music_handler)
