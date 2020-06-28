4d0946f52a48ab4c99589df5f05b0347f0fe9197
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 22:16:46 +0530

    Update grammer.py

--- alluka/modules/grammer.py
+++ alluka/modules/grammer.py
@@ -52,7 +52,7 @@ __help__ = """
  - /t: while replying to a message, will reply with a grammar corrected version
 """
 
-__mod_name__ = "GRAMMER/TRANSLATOR"
+__mod_name__ = "GRAMMER"
 
 
 TRANSLATE_HANDLER = CommandHandler('t', translate)
