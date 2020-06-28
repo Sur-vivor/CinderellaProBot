de667a77654e820f9cb03a2d67f6d3e498085b82
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-06-28 21:16:26 +0530

    Update blacklist.py

--- alluka/modules/blacklist.py
+++ alluka/modules/blacklist.py
@@ -193,5 +193,5 @@ dispatcher.add_handler(ADD_BLACKLIST_HANDLER)
 dispatcher.add_handler(UNBLACKLIST_HANDLER)
 dispatcher.add_handler(BLACKLIST_DEL_HANDLER, group=BLACKLIST_GROUP)
 
-__mod_name__ = "Word Blacklists"
+__mod_name__ = "WORD BLACKLISTS"
 __handlers__ = [BLACKLIST_HANDLER, ADD_BLACKLIST_HANDLER, UNBLACKLIST_HANDLER, (BLACKLIST_DEL_HANDLER, BLACKLIST_GROUP)]
