59650d37d0b0ba602b07ff5dc22644316507b959
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-02 13:10:58 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -284,7 +284,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Sur_vivor)]]))
+                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Sur_vivor")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
