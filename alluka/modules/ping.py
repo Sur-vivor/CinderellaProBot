6b2d741209162419697910e6a16ac25ad54edfd8
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-13 22:36:34 +0530

    Update ping.py

--- alluka/modules/ping.py
+++ alluka/modules/ping.py
@@ -53,7 +53,7 @@ def ping(bot: Bot, update: Update):
     requests.get('https://api.telegram.org')
     end_time = time.time()
     ms = float(end_time - start_time)
-    update.effective_message.reply_text("🏓 Pong!\n⏱️Reply took: {0:.2f}s".format(round(ms, 2) % 60), parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_text("🏓 Pong!\n⏱️Reply took: {0:.2f}s\nService Uptime: {}".format((round(ms, 2) % 60), uptime), parse_mode=ParseMode.MARKDOWN)
 
 @run_async
 def uptime(bot: Bot, update: Update):
