7361ec44f807436f671939bafcdc2359a1d4f02f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-14 21:52:36 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -591,9 +591,6 @@ def main():
 
     # dispatcher.add_error_handler(error_callback)
 
-    # add antiflood processor
-    Dispatcher.process_update = process_update
-
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
         updater.start_webhook(listen="127.0.0.1",
