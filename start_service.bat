2e28922bb18a60d72dffd68c395aae69efce91b4
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-19 18:37:32 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -24,5 +24,5 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-net stop CinderellaBot
-net start CinderellaBot
+net stop CinderellaProBot
+net start CinderellaProBot
