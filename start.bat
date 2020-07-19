898e38a2c3f779c4521d505fcff6f0b6ab11a9b3
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-19 18:36:40 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,12 @@
 @echo off
-TITLE CinderellaBot
+TITLE CinderellaProBot
 rem This next line removes any fban csv files if they exist in root when bot restarts. 
 del *.csv
 py -3.7 --version
 IF "%ERRORLEVEL%" == "0" (
-    py -3.7 -m alluka
+    py -3.7 -m cinderella
 ) ELSE (
-    py -m alluka
+    py -m cinderella
 )
 
 pause
