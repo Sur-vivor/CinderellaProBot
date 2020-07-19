e757787f7d0ce1876648399ffb564bc89c0c174d
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-19 16:57:51 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -31,7 +31,7 @@ from alluka.modules.connection import connect_button
 PM_START_TEXT = """
 _Hello_ *{}*
 _My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
-_Maintained by_ [⚡{}⚡](tg://user?id={})
+*OWNER* [⚡{}⚡](tg://user?id={})
 """
 
 
@@ -183,8 +183,9 @@ def send_start(bot, update):
 
     keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/Sur_vivor")]]
     keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
-
-    update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_USERNAME, OWNER_ID), 
+    keyboard += [[InlineKeyboardButton(text="👽Owner",url="https://t.me/{OWNER_USERNAME})]]
+    
+    update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
