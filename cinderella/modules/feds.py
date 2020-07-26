554e6c2da6dc18f89234b3ca302f7a70a6cb355f
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-07-26 23:22:59 +0530

    Update feds.py

--- cinderella/modules/feds.py
+++ cinderella/modules/feds.py
@@ -1604,20 +1604,19 @@ def is_user_fed_owner(fed_id, user_id):
     else:
         return False
 
-@run_async
 def welcome_fed(bot, update):
-	chat = update.effective_chat  
-	user = update.effective_user  
-	fed_id = sql.get_fed_id(chat.id)
-	fban, fbanreason, fbantime = sql.get_fban_user(fed_id, user.id)
-	if fban:
-		send_message(update.effective_message, "This user is banned in current federation! I will remove him.")
-		bot.kick_chat_member(chat.id, user.id)
-		return True
-	else:
-		return False
-        
-	
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+
+    fed_id = sql.get_fed_id(chat.id)
+    fban = fban = sql.get_fban_user(fed_id, user.id)
+    if not fban == False:
+        update.effective_message.reply_text(chat.id, "This user if banned in current federation! I will remove him.")
+        bot.kick_chat_member(chat.id, user.id)
+        return True
+    else:
+        return False
+        	
 def __stats__():
     all_fbanned = sql.get_all_fban_users_global()
     all_feds = sql.get_all_feds_users_global()
