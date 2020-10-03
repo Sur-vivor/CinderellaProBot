4a8eed4c435e5c55bae1598b77ffe18d39648b71
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-10-03 18:00:08 +0530

    Update feds.py

--- cinderella/modules/feds.py
+++ cinderella/modules/feds.py
@@ -73,16 +73,18 @@ def new_fed(bot: Bot, update: Update):
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
     message = update.effective_message
- 
+    fed_id = sql.get_fed_id(chat.id)
+    info = sql.get_fed_info(fed_id)
+    get_owner = eval(info['fusers'])['owner']
+     
     if chat.type != "private":
         update.effective_message.reply_text("Please run this command in my PM only!")
         return
 
-    fed_id = sql.get_fed_id(chat.id)
-    info = sql.get_fed_info(fed_id)
-    get_owner = eval(info['fusers'])['owner']	
     if user.id == get_owner:
-	update.effective_message.reply_text("Only one federation per admin.")
+		update.effective_message.reply_text("Only one federation per admin.")
+		return
+	
     fednam = message.text.split(None, 1)[1]
     if not fednam == '':
         fed_id = str(uuid.uuid4())
