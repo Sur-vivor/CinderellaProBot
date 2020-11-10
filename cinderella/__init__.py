1079748d40958b8e917fca89ba1a72dc7040575a
Mughilethi <72306179+Mughileth@users.noreply.github.com>
2020-11-10 19:49:16 +0530

    U

--- cinderella/__init__.py
+++ cinderella/__init__.py
@@ -30,7 +30,7 @@ if ENV:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
     MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
-    OWNER_NAME = os.environ.get("OWNER_NAME", mughilethi)
+    OWNER_NAME = os.environ.get("OWNER_NAME", mughileth)
 
     try:
         SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
