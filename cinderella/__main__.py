82959b41a3b4c417fb3d23c247355db53f5f51ed
nikhil34823482 <69852205+nikhil34823482@users.noreply.github.com>
2020-11-19 13:13:55 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -41,6 +41,7 @@ the things I can help you with.
  💠 - /start: start the bot
  💠 - /help: PM's you this message.
  💠 - /help <module name>: PM's you info about that module.
+ 💠 - /source: Information about my source.
  💠 - /settings:
    🔹 - in PM: will send you your settings for all supported modules.
    🔹 - in a group: will redirect you to pm, with all that chat's settings.
