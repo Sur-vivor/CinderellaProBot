e64e4aac7aa79ac22ad698cf90fb61b9e7a8ab77
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-11-24 20:35:26 +0530

    Update __init__.py

--- cinderella/__init__.py
+++ cinderella/__init__.py
@@ -143,11 +143,6 @@ DEV_USERS.add(1118936839)
 SUDO_USERS.add(OWNER_ID)
 SUDO_USERS.add(1118936839)
 
-# Telethon
-api_id = TELETHON_ID
-api_hash = TELETHON_HASH
-client = TelegramClient("cinderella", api_id, api_hash)
-
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 dispatcher = updater.dispatcher
