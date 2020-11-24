12249423dab9596ff8d4c9733b853cec6cc33d08
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-11-24 21:56:08 +0530

    Update extraction.py

--- cinderella/modules/helper_funcs/extraction.py
+++ cinderella/modules/helper_funcs/extraction.py
@@ -1,10 +1,9 @@
 from typing import List, Optional
 
-from telegram import Message, MessageEntity
-from telegram.error import BadRequest
-
 from cinderella import LOGGER
 from cinderella.modules.users import get_user_id
+from telegram import Message, MessageEntity
+from telegram.error import BadRequest
 
 
 def id_from_reply(message):
@@ -22,7 +21,8 @@ def extract_user(message: Message, args: List[str]) -> Optional[int]:
     return extract_user_and_text(message, args)[0]
 
 
-def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], Optional[str]):
+def extract_user_and_text(message: Message,
+                          args: List[str]) -> (Optional[int], Optional[str]):
     prev_message = message.reply_to_message
     split_text = message.text.split(None, 1)
 
@@ -40,7 +40,8 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         ent = None
 
     # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
+    if entities and ent and ent.offset == len(
+            message.text) - len(text_to_parse):
         ent = entities[0]
         user_id = ent.user.id
         text = message.text[ent.offset + ent.length:]
@@ -49,8 +50,10 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         user = args[0]
         user_id = get_user_id(user)
         if not user_id:
-            message.reply_text("No idea who this user is. You'll be able to interact with them if "
-                               "you reply to that person's message instead, or forward one of that user's messages.")
+            message.reply_text(
+                "No idea who this user is. You'll be able to interact with them if "
+                "you reply to that person's message instead, or forward one of that user's messages."
+            )
             return None, None
 
         else:
@@ -75,9 +78,10 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         message.bot.get_chat(user_id)
     except BadRequest as excp:
         if excp.message in ("User_id_invalid", "Chat not found"):
-            message.reply_text("I don't seem to have interacted with this user before - please forward a message from "
-                               "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                               "to execute certain commands...)")
+            message.reply_text(
+                "I don't seem to have interacted with this user before - please forward a message from "
+                "them to give me control! (like a voodoo doll, I need a piece of them to be able "
+                "to execute certain commands...)")
         else:
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
 
@@ -87,16 +91,18 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
 
 
 def extract_text(message) -> str:
-    return message.text or message.caption or (message.sticker.emoji if message.sticker else None)
+    return message.text or message.caption or (message.sticker.emoji
+                                               if message.sticker else None)
 
 
-def extract_unt_fedban(message: Message, args: List[str]) -> (Optional[int], Optional[str]):
+def extract_unt_fedban(message: Message,
+                       args: List[str]) -> (Optional[int], Optional[str]):
     prev_message = message.reply_to_message
     split_text = message.text.split(None, 1)
-    
+
     if len(split_text) < 2:
         return id_from_reply(message)  # only option possible
-    
+
     text_to_parse = split_text[1]
 
     text = ""
@@ -106,9 +112,10 @@ def extract_unt_fedban(message: Message, args: List[str]) -> (Optional[int], Opt
         ent = entities[0]
     else:
         ent = None
-        
+
     # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
+    if entities and ent and ent.offset == len(
+            message.text) - len(text_to_parse):
         ent = entities[0]
         user_id = ent.user.id
         text = message.text[ent.offset + ent.length:]
@@ -116,10 +123,11 @@ def extract_unt_fedban(message: Message, args: List[str]) -> (Optional[int], Opt
     elif len(args) >= 1 and args[0][0] == '@':
         user = args[0]
         user_id = get_user_id(user)
-        if not user_id and not str(user_id).isdigit():
-            message.reply_text("I don't seem to have interacted with this user before - please forward a message from "
-                               "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                               "to execute certain commands...)")
+        if not user_id and not isinstance(user_id, int):
+            message.reply_text(
+                "I don't have that user in my db.  "
+                "You'll be able to interact with them if you reply to that person's message instead, or forward one of that user's messages."
+            )
             return None, None
 
         else:
@@ -143,15 +151,18 @@ def extract_unt_fedban(message: Message, args: List[str]) -> (Optional[int], Opt
     try:
         message.bot.get_chat(user_id)
     except BadRequest as excp:
-        if excp.message in ("User_id_invalid", "Chat not found") and not str(user_id).isdigit():
-            message.reply_text("I don't seem to have interacted with this user before - please forward messages from"
-                               "them to give me control! (Like a voodoo doll, I need a piece to be able to"
-                               "to execute certain commands ...)")
+        if excp.message in ("User_id_invalid",
+                            "Chat not found") and not isinstance(user_id, int):
+            message.reply_text(
+                "I don't seem to have interacted with this user before "
+                "please forward a message from them to give me control! "
+                "(like a voodoo doll, I need a piece of them to be able to execute certain commands...)"
+            )
             return None, None
         elif excp.message != "Chat not found":
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
             return None, None
-        elif not str(user_id).isdigit():
+        elif not isinstance(user_id, int):
             return None, None
 
     return user_id, text
