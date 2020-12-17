4a6ad15608509ea4b6b26156a4b84a17bfc65206
piku-adhi <73607487+piku-adhi@users.noreply.github.com>
2020-12-17 17:50:17 +0530

    __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -39,13 +39,13 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
- 💠 - /start: start the bot
- 💠 - /help: PM's you this message.
- 💠 - /help <module name>: PM's you info about that module.
- 💠 - /source: Information about my source.
- 💠 - /settings:
-   🔹 - in PM: will send you your settings for all supported modules.
-   🔹 - in a group: will redirect you to pm, with all that chat's settings.
+ 🔆- /start: start the bot
+ 🔱 - /help: PM's you this message.
+ ▶️- /help <module name>: PM's you info about that module.
+ ⏩ - /source: clone me .
+ 🗡️- /settings:
+   ➡️ - in PM: will send you your settings for all supported modules.
+   ♒- in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
@@ -59,7 +59,7 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [there](https://t.me/http://t.me/Bot_Hub1)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [there](https://t.me/Bot_Hub1)
 ⚡You Can Clone Me [Here](https://github.com/piku-adhi/DUHO)
 """
 
