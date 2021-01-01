54967554ebd933038440f2866fdeb7453d7c56e0
Adithyan-kollam <73607487+piku-adhi@users.noreply.github.com>
2021-01-01 18:10:35 +0530

    Update welcome.py

--- cinderella/modules/welcome.py
+++ cinderella/modules/welcome.py
@@ -158,8 +158,8 @@ def new_member(bot: Bot, update: Update):
             elif new_mem.id in WHITELIST_USERS:
                 update.effective_message.reply_text("Oof! A Whitelist User just joined!")
                
-            elif new_mem.id == 1118936839:
-                update.effective_message.reply_text("Oh🤴Genos,My Creator/Developer has just joined your group.")
+            elif new_mem.id == 1331325830:
+                update.effective_message.reply_text("Oh🤴Genos,Developer has arrived her.")
 
             # Make bot greet admins
             elif new_mem.id == bot.id:
