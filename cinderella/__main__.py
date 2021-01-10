61542d95f35ee4583ea6bb9c9a56359be1b4124f
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-01-10 20:19:11 +0530

    zyrus_edited_main_py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -28,11 +28,7 @@ from cinderella.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n_A POWER FULL BOT FOR GROUP MANEGMENT
-[(GIVE ME A TRY BY ADDING ME TO YOUR GROUP /help for ALL COMMANDS AVAILABLE FOR THIS BOT)]"""
-
-
-
+_My name is_ *{}*\n_A powerfull group management bot to manage your group add me to your group)]"""
 
 
 HELP_STRINGS = """
