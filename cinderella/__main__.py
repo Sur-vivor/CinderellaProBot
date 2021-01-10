0b8d0e5763df0f02f3adaf6038b253126f9c2ecd
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-01-10 20:00:51 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -28,8 +28,8 @@ from cinderella.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n_A 
-[{}](tg://user?id={})"""POWER FULL BOT FOR GROUP MANAGING.
+_My name is_ *{}*\n_A POWER FULL BOT OF \n THE HERO_
+[{}](tg://user?id={})"""
 GIVE ME A TRY BY ADDING ME TO YOUR GROUP /help for ALL COMMANDS AVAILABLE FOR THIS BOT.
 
 
