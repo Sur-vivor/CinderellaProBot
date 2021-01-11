701f914002682f41d8295d4c6e05ffdbb23c5724
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-01-11 21:57:08 +0530

    Update system_info.py

--- cinderella/modules/system_info.py
+++ cinderella/modules/system_info.py
@@ -102,7 +102,8 @@ def status(bot: Bot, update: Update):
 	memm += f"Percentage: `{svmem.percent}%`\n"
 	reply = str(stat)+ str(softw) + str(cpuu) + str(memm) + "\n"
 	bot.send_message(chat.id, reply, parse_mode=ParseMode.MARKDOWN)        
-
+        # bot info
+	" THIS BOT IS CURRENTLY UPDATED WITH MASTER! FOR ANY QUERY CONTACT DEVOLEPER [HERE](https://t.me/ram_jii)"
 __help__ = """
 - /system : To know System status
 - /speed or - /speedtest: To find Speed
