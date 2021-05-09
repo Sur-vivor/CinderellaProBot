ddcca166e609ef48f9340216ca37c9570ece0430
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-05-09 16:49:37 +0530

    Update night_mode_sql.py

--- cinderella/modules/sql_extended/night_mode_sql.py
+++ cinderella/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from MashaRoBot.modules.sql import BASE, SESSION
+from cinderella.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
